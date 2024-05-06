f2b54555224d07702848ba990fc1ee3f1636e5ff
zfersya <zfersya@gmail.com>
2024-05-06 13:59:05 +0700

    first commit

---
+++ AljabarLibraries/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Perhitungan
+    {
+        public double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+            double d = b*b - (4 * a * c);
+
+            if(d < 0)
+            {
+                return new double[0];
+            }
+            else if(d > 0)
+            {
+                double e = Math.Sqrt(d);
+                return new double[] { (-b + e) / (2 * a), (-b - e) / (2 * a) };
+            }
+            else
+            {
+                return new double[] {-b / (2 * a)};
+            }
+        }
+        public double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            
+            double aKuadrat = Math.Pow(a,2);
+            double ab = 2 * a * b;
+            double bKuadrat = Math.Pow(b,2);
+
+            return new double[] { aKuadrat, ab, bKuadrat };
+        }
+    }
+}
---
+++ tpmodul10_1302223090_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace tpmodul10_1302223090_3 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Perhitungan kalkulator = new Perhitungan();
+
+            double[] akar = kalkulator.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine("\nAkar persamaan kuadrat\t: ");
+            for (int i = 0; i < akar.Length; i++)
+            {
+                Console.Write(akar[i]);
+                Console.Write("  ");
+            }
+            Console.WriteLine("  ");
+            
+            Console.WriteLine("  ");
+            double[] hasilKuadrat = kalkulator.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine("Hasil hitung kuadrat\t: ");
+            for (int i = 0; i < hasilKuadrat.Length; i++)
+            {
+                Console.Write(hasilKuadrat[i]);
+                Console.Write("  ");
+            }
+            Console.WriteLine("  ");
+            Console.ReadKey();
+            
+        }
+    }
+}
